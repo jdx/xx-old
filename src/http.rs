739475a13dab40a9029309992493cd08e9772d98
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-09-22 18:40:34 -0700

    remove fs

--- src/http.rs
+++ src/http.rs
@@ -15,24 +15,22 @@ pub enum DownloadError {
 /// # Examples
 ///
 /// ```
-/// use std::fs::File;
+/// use std::fs;
 /// use std::io::prelude::*;
 ///
-/// fn main() -> Result<(), xx::http::DownloadError> {
-///     xx::http::download("https://jsonplaceholder.typicode.com/todos/1", "tmp/todo.json")?;
-///     let contents = xx::fs::read_file("tmp/todo.json").expect("error reading file");
+/// fn main() {
+///     xx::http::download("https://jsonplaceholder.typicode.com/todos/1", "tmp/todo.json")
+///       .expect("error downloading");
+///     let contents = fs::read_to_string("tmp/todo.json").expect("error reading file");
 ///     assert_eq!(contents, "{\n  \"userId\": 1,\n  \"id\": 1,\n  \"title\": \"delectus aut autem\",\n  \"completed\": false\n}");
-///     Ok(())
 /// }
 /// ```
 pub fn download(url: &str, to: &str) -> Result<(), DownloadError> {
     println!("downloading {} to {}", url, to);
     let mut response = reqwest::get(url).map_err(DownloadError::HTTP)?;
     let parent_path = Path::new(to).parent().expect("invalid path");
-    fs::create_dir_all(parent_path)
-        .map_err(DownloadError::IO)?;
+    fs::create_dir_all(parent_path).map_err(DownloadError::IO)?;
     let mut file = fs::File::create(to).map_err(DownloadError::IO)?;
-    io::copy(&mut response, &mut file)
-        .map_err(DownloadError::IO)?;
+    io::copy(&mut response, &mut file).map_err(DownloadError::IO)?;
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
 pub mod base64;
-pub mod fs;
 pub mod hash;
 pub mod http;
